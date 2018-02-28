bc50a0cdbc52febc72d5c625080ce22f28bfec07
miguelfmlopes91 <miguelpvz@hotmail.com>
2018-02-28 23:23:49 +0000

    Got endpoints for call and api key

---
+++ java/com/bardo/foodnearby/rest/ApiInterface.java
@@ -0,0 +1,21 @@
+package com.bardo.foodnearby.rest;
+
+/**
+ * Created by Miguel on 28.02.2018.
+ */
+
+import com.bardo.foodnearby.model.MoviesResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+
+public interface ApiInterface {
+    @GET("movie/top_rated")
+    Call<MoviesResponse> getTopRatedMovies(@Query("api_key") String apiKey);
+
+    @GET("movie/{id}")
+    Call<MoviesResponse> getMovieDetails(@Path("id") int id, @Query("api_key") String apiKey);
+}
