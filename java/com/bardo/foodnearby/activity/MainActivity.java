1b4af9657398ea57a5cc86394c0d80c5b17ddefd
miguelfmlopes91 <miguelpvz@hotmail.com>
2018-03-01 00:24:56 +0000

    added api key

--- java/com/bardo/foodnearby/activity/MainActivity.java
+++ java/com/bardo/foodnearby/activity/MainActivity.java
@@ -1,11 +1,15 @@
 package com.bardo.foodnearby.activity;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.bardo.foodnearby.R;
+import com.bardo.foodnearby.adapter.MoviesAdapter;
 import com.bardo.foodnearby.model.Movie;
 import com.bardo.foodnearby.model.MoviesResponse;
 import com.bardo.foodnearby.rest.ApiClient;
@@ -17,11 +21,13 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+
 public class MainActivity extends AppCompatActivity {
+
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    // TODO - insert your themoviedb.org API KEY here
-    private final static String API_KEY = "";
+
+    private final static String API_KEY = "32e2e59f425895fce35562c206d0f550";
 
 
     @Override
@@ -30,9 +36,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         if (API_KEY.isEmpty()) {
-            Toast.makeText(getApplicationContext(), "Please obtain your API KEY first from themoviedb.org", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Please obtain your API KEY from themoviedb.org first!", Toast.LENGTH_LONG).show();
             return;
         }
+
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.movies_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         ApiInterface apiService =
                 ApiClient.getClient().create(ApiInterface.class);
@@ -40,13 +49,14 @@ public class MainActivity extends AppCompatActivity {
         Call<MoviesResponse> call = apiService.getTopRatedMovies(API_KEY);
         call.enqueue(new Callback<MoviesResponse>() {
             @Override
-            public void onResponse(Call<MoviesResponse>call, Response<MoviesResponse> response) {
+            public void onResponse(Call<MoviesResponse> call, Response<MoviesResponse> response) {
+                int statusCode = response.code();
                 List<Movie> movies = response.body().getResults();
-                Log.d(TAG, "Number of movies received: " + movies.size());
+                recyclerView.setAdapter(new MoviesAdapter(movies, R.layout.list_item_movie, getApplicationContext()));
             }
 
             @Override
-            public void onFailure(Call<MoviesResponse>call, Throwable t) {
+            public void onFailure(Call<MoviesResponse> call, Throwable t) {
                 // Log error here since request failed
                 Log.e(TAG, t.toString());
             }
